32bce60d7eb8cb6484b0523c590fd4b00a2b4896
SeanChinJunKai <e0774037@u.nus.edu>
2025-05-20 20:09:34 +0800

    Add firebase-ios-sdk depedency and FirebaseVertexAI class

---
+++ FirebaseVertexAIBridge/FirebaseVertexAIBridge-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  FirebaseVertexAIBridge-Bridging-Header.h
+//  FirebaseVertexAIBridge
+//
+//  Created by Sean Chin on 20/5/25.
+//
+
--- FirebaseVertexAIBridge/FirebaseVertexAIBridge.swift
+++ FirebaseVertexAIBridge/FirebaseVertexAIBridge.swift
@@ -2,9 +2,16 @@
 //  FirebaseVertexAIBridge.swift
 //  FirebaseVertexAIBridge
 //
-//  Created by Sean  Chin on 20/5/25.
+//  Created by Sean Chin on 20/5/25.
 //
+import Foundation
+import FirebaseVertexAI
 
-class FirebaseVertexAIBridge {
-
+@objc(FirebaseVertexAI)
+public class FirebaseVertexAI: NSObject {
+    private let service: VertexAI
+    
+    @objc public override init() {
+        self.service = VertexAI.vertexAI()
+    }
 }
