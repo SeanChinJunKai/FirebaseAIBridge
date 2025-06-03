37bedd779dbcfca377b660a2fa40386f59a3caca
SeanChinJunKai <82928860+SeanChinJunKai@users.noreply.github.com>
2025-06-03 17:01:28 +0800

    Refactor enum classes to be normal classes (#9)
    * Refactor enum classes to be normal classes
    
    * Change HarmCategory property to be rawValue for standardisation

--- FirebaseAIBridge/Sources/FinishReason.swift
+++ FirebaseAIBridge/Sources/FinishReason.swift
@@ -7,41 +7,14 @@
 import Foundation
 import FirebaseAI
 
-@objc public enum FinishReasonObjc: Int {
-    case unknown = 0
-    case stop = 1
-    case maxTokens = 2
-    case safety = 3
-    case recitation = 4
-    case other = 5
-    case blocklist = 6
-    case prohibitedContent = 7
-    case spii = 8
-    case malformedFunctionCall = 9
+@objcMembers
+public class FinishReasonObjc: NSObject {
+    public let rawValue: String
     
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
     public static func from(_ finishReason: FinishReason) -> FinishReasonObjc {
-        switch finishReason.rawValue {
-        case FinishReason.stop.rawValue:
-            return .stop
-        case FinishReason.maxTokens.rawValue:
-            return .maxTokens
-        case FinishReason.safety.rawValue:
-            return .safety
-        case FinishReason.recitation.rawValue:
-            return .recitation
-        case FinishReason.other.rawValue:
-            return .other
-        case FinishReason.blocklist.rawValue:
-            return .blocklist
-        case FinishReason.prohibitedContent.rawValue:
-            return .prohibitedContent
-        case FinishReason.spii.rawValue:
-            return .spii
-        case FinishReason.malformedFunctionCall.rawValue:
-            return .malformedFunctionCall
-        default:
-            return .unknown
-        }
+        return FinishReasonObjc(rawValue: finishReason.rawValue)
     }
 }
-
--- FirebaseAIBridge/Sources/HarmCategory.swift
+++ FirebaseAIBridge/Sources/HarmCategory.swift
@@ -7,28 +7,15 @@
 import Foundation
 import FirebaseAI
 
-@objc public enum HarmCategoryObjc: Int {
-    case unknown = 0
-    case harassment = 1
-    case hateSpeech = 2
-    case sexuallyExplicit = 3
-    case dangerousContent = 4
-    case civicIntegrity = 5
+@objcMembers
+public class HarmCategoryObjc: NSObject {
+    public let rawValue: String
+    
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
     
     public static func from(_ harmCategory: HarmCategory) -> HarmCategoryObjc {
-        switch harmCategory.rawValue {
-        case HarmCategory.harassment.rawValue:
-            return .harassment
-        case HarmCategory.hateSpeech.rawValue:
-            return .hateSpeech
-        case HarmCategory.sexuallyExplicit.rawValue:
-            return .sexuallyExplicit
-        case HarmCategory.dangerousContent.rawValue:
-            return .dangerousContent
-        case HarmCategory.civicIntegrity.rawValue:
-            return .civicIntegrity
-        default:
-            return .unknown
-        }
+        return HarmCategoryObjc(rawValue: harmCategory.rawValue)
     }
 }
