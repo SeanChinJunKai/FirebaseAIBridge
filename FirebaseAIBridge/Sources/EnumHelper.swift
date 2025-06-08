4be89092cec7945cd5faff4fd0a8a97e36207017
SeanChinJunKai <e0774037@u.nus.edu>
2025-06-08 22:07:44 +0800

    Test debug

---
+++ FirebaseAIBridge/Sources/EnumHelper.swift
@@ -0,0 +1,13 @@
+//
+//  EnumHelper.swift
+//  FirebaseAIBridge
+//
+//  Created by Sean  Chin on 8/6/25.
+//
+import Foundation
+
+@objc public class EnumHelper: NSObject {
+    @objc public static func getEnumValue() -> FirebaseAIErrorObjc {
+        return .contentBlocked
+    }
+}
