fff89a1079e4c9c538563453f9cf22a3da489748
SeanChinJunKai <e0774037@u.nus.edu>
2025-06-08 20:34:09 +0800

    Debug 3

---
+++ FirebaseAIBridge/Sources/DEF.swift
@@ -0,0 +1,20 @@
+//
+//  DEF.swift
+//  FirebaseAIBridge
+//
+//  Created by Sean  Chin on 8/6/25.
+//
+
+import Foundation
+
+@objc public enum DEFObjc: Int {
+    case server = 0
+    case promptBlocked = 1
+    case responseStopped = 2
+    case serialization = 3
+    case invalidAPIKey = 4
+    case quotaExceeded = 5
+    case unsupportedUserLocation = 6
+    case serviceDisabled = 7
+    case contentBlocked = 8
+}
