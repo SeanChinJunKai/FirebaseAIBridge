4f6ca7141476f9e289e64d2d30f057ffdc398591
SeanChinJunKai <e0774037@u.nus.edu>
2025-06-08 20:24:04 +0800

    Debug

---
+++ FirebaseAIBridge/Sources/ABC.swift
@@ -0,0 +1,13 @@
+//
+//  ABC.swift
+//  FirebaseAIBridge
+//
+//  Created by Sean  Chin on 8/6/25.
+//
+import Foundation
+
+@objc public class ABCObjc: NSObject {
+    @objc public override init() {
+        super.init()
+    }
+}
