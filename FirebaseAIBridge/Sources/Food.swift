406c6493419f6b075d3896d05c2aec6fcb59ce62
SeanChinJunKai <e0774037@u.nus.edu>
2025-06-08 21:52:03 +0800

    Test debug

---
+++ FirebaseAIBridge/Sources/Food.swift
@@ -0,0 +1,12 @@
+//
+//  Food.swift
+//  FirebaseAIBridge
+//
+//  Created by Sean  Chin on 8/6/25.
+//
+
+@objc public enum Food: Int {
+    case breakfast = 0
+    case lunch = 1
+    case dinner = 2
+}
