122bd1d9bea9f1aef72dfa9bb64c6b1257f3c84a
SeanChinJunKai <e0774037@u.nus.edu>
2025-06-10 01:37:06 +0800

    Test again

--- FirebaseAIBridge/Sources/FirebaseAIBridge-Bridging-Header.h
+++ FirebaseAIBridge/Sources/FirebaseAIBridge-Bridging-Header.h
@@ -5,4 +5,17 @@
 //  Created by Sean Chin on 20/5/25.
 //
 
-#import "FirebaseAIError.h"
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, FirebaseAIErrorObjc) {
+    FirebaseAIErrorObjcServer = 0,
+    FirebaseAIErrorObjcPromptBlocked = 1,
+    FirebaseAIErrorObjcResponseStopped = 2,
+    FirebaseAIErrorObjcSerialization = 3,
+    FirebaseAIErrorObjcInvalidAPIKey = 4,
+    FirebaseAIErrorObjcQuotaExceeded = 5,
+    FirebaseAIErrorObjcUnsupportedUserLocation = 6,
+    FirebaseAIErrorObjcServiceDisabled = 7,
+    FirebaseAIErrorObjcContentBlocked = 8
+};
+
