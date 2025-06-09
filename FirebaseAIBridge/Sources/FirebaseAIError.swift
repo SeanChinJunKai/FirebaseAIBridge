c8a660fb8ebeb68e5df1a480b0f6d5952daafd2e
SeanChinJunKai <e0774037@u.nus.edu>
2025-06-10 01:10:45 +0800

    Test again

--- FirebaseAIBridge/Sources/FirebaseAIError.swift
+++ FirebaseAIBridge/Sources/FirebaseAIError.swift
@@ -6,17 +6,6 @@
 //
 import Foundation
 
-@objc public class AIError: NSObject {
-    @objc public let error: NSError
-    
-    @objc public let errorCode: FirebaseAIErrorObjc
-    
-    @objc public init(error: NSError, errorCode: FirebaseAIErrorObjc) {
-        self.error = error
-        self.errorCode = errorCode
-    }
-}
-
 @objc public enum FirebaseAIErrorObjc: Int {
     case server = 0
     case promptBlocked = 1
